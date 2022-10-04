74bc56f1170ac788e977afe7ea65ecadd7ac27f7
omprakashpandit <32616762+omprakashpandit@users.noreply.github.com>
2022-10-04 21:21:21 +0530

    Add files via upload

---
+++ spring-boot-logging-ymlfile/src/main/java/com/javatechie/CourseServiceApplication.java
@@ -0,0 +1,23 @@
+package com.javatechie;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseServiceApplication {
+private static final Logger logger=LoggerFactory.getLogger(CourseServiceApplication.class);
+	public static void main(String[] args) {
+		logger.info("Starting");
+		SpringApplication.run(CourseServiceApplication.class, args);
+		for(int i=1;i<10000;i++) {
+		logger.info("INFO/DEBUG/TRACE-green");
+		logger.error("ERRROR/FATAL-red");
+		logger.warn("WARN-yellow");
+		logger.info("End.");
+		}
+	}
+
+}
